8abb8798da864c6d9c20ca0be3806344f5c2f963
hodakaaaa <np03cs4s220391@heraldcollege.edu.np>
2024-09-10 19:19:52 +0545

    StarRating

---
+++ star_rating/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import StarRating from './components/star-rating';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Star Rating Component */}
+      <StarRating noOfStars={10}/>
+    </div>
+  );
+}
+
+export default App;
