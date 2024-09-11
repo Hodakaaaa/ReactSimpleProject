e5459bcc0c815c32914fc0941f08f7981e59e830
hodakaaaa <np03cs4s220391@heraldcollege.edu.np>
2024-09-11 20:15:30 +0545

    Load More Content

---
+++ load_more_data/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import LoadMoreData from './components/load-more-data';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Load More Products Component */}
+      <LoadMoreData/>
+    </div>
+  );
+}
+
+export default App;
