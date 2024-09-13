874f0011afda77a31c32729c3b76d9345e14657a
hodakaaaa <np03cs4s220391@heraldcollege.edu.np>
2024-09-13 10:52:05 +0545

    ThemeChange

---
+++ theme_change/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import LightDarkMode from './components/dark-mode';
+
+function App() {
+  return (
+    <div className="App">
+      <LightDarkMode/>
+    </div>
+  );
+}
+
+export default App;
