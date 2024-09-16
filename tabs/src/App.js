b235eb7b01409826af6c5af334421b68b8a3bf0a
hodakaaaa <np03cs4s220391@heraldcollege.edu.np>
2024-09-16 08:58:40 +0545

    tabs

---
+++ tabs/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import TabTest from './Components/tabs/tab-test';
+
+function App() {
+  return (
+    <div className="App">
+     {/* Tabs Components */}
+     <TabTest/>
+    </div>
+  );
+}
+
+export default App;
