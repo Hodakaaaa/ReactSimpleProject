1432d43aad3d23395ee54554e8a0c2c096689e98
hodakaaaa <np03cs4s220391@heraldcollege.edu.np>
2024-09-19 08:42:21 +0545

    Tic Tac Toe

---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import TicTacToe from './components/tic-tac-toe';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Tic Tac Toe Component */}
+      <TicTacToe/>
+    </div>
+  );
+}
+
+export default App;
