52ba9dc8aad545175d82d2d00344373127230a13
mandhoj <np03cs4s220391@heraldcollege.edu.np>
2024-10-21 09:30:31 +0545

    useFetch Custom Hook

---
+++ customhook/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import UseFetchHookTest from './components/test';
+
+
+function App() {
+  return (
+    <div className="App">
+
+      {/* use-fecth- Custom Hook */}
+      <UseFetchHookTest/>
+      
+    </div>
+  );
+}
+
+export default App;
